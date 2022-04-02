9e2450a923d7f443ebe7ddf0577520208e7068a1
antor10655gh <creativefreelancerantor@gmail.com>
2022-04-03 01:19:44 +0600

    add login-form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import ItService from "./components/ItService/ItService";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,17 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const showLoginForm = () => {
+    const loginForm = document.getElementById("loginform");
+    loginForm.style.display = "block";
+    loginForm.style.top = "50px";
+  };
+  const hideLoginForm = () => {
+    const loginForm = document.getElementById("loginform");
+    loginForm.style.display = "none";
+  };
   return (
-    <div className="container">
+    <div className="container header">
       <div className="row d-flex justify-content-center align-items-center mt-3">
         <div className="col">
           <img
@@ -25,10 +34,34 @@ const Header = () => {
           </nav>
         </div>
         <div className="col">
-          <button>
+          <button onClick={showLoginForm}>
             <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
             <span>Log-in</span>
           </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="login-container" id="loginform">
+            <h3 className="pt-3">Log-in</h3>
+            <div className="login-form">
+              <label htmlFor="" style={{ marginTop: "20px" }}>
+                Email
+              </label>
+              <input type="text" placeholder="Enter your email" />
+              <label htmlFor="" style={{ marginTop: "15px" }}>
+                Password
+              </label>
+              <input
+                style={{ marginBottom: "15px" }}
+                type="text"
+                placeholder="Enter your password"
+              />
+              <button onClick={hideLoginForm} className="login-btn">
+                Login
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
